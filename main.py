496408de2a486513b07bb39ca0aa5065381a0901
kellyjj <kellyjj@yahoo.com>
2023-04-11 15:07:48 -0700

    scrolling left right done

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ class Object(pygame.sprite.Sprite):
         self.height = height
         self.name = name
 
-    def draw(self,win):
-        win.blit(self.image,(self.rect.x,self.rect.y))
+    def draw(self,win,offset_x):
+        win.blit(self.image,(self.rect.x-offset_x,self.rect.y))
 
 class Block(Object):
     def __init__(self, x, y, size):
@@ -104,7 +104,7 @@ class Player(pygame.sprite.Sprite):
         self.jump_count =0 
 
     def jump(self):
-        self.y_vel = -self.GRAVITY * 24
+        self.y_vel = -self.GRAVITY * 16
         self.animation_count =0 
         self.jump_count +=1
         if self.jump_count==1:
@@ -162,6 +162,13 @@ class Player(pygame.sprite.Sprite):
         sprite_sheet = "idle"
         if self.x_vel!=0:
             sprite_sheet = "run"
+        if self.y_vel<0:
+            if self.jump_count==1:
+                sprite_sheet="jump"
+            elif self.jump_count==2:
+                sprite_sheet="double_jump"
+        elif self.y_vel >self.GRAVITY*2:
+            sprite_sheet = "fall"
 
         sprite_sheet_name = sprite_sheet + "_"+self.direction
         sprites = self.SPRITES[sprite_sheet_name]
@@ -174,12 +181,12 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.sprite.get_rect(topleft=(self.rect.x,self.rect.y))
         self.mask = pygame.mask.from_surface(self.sprite)
 
-    def draw(self, win):
+    def draw(self, win,offset_x):
         # pygame.draw.rect(win,self.COLOR,self.rect)
         # win.blit(self.sprite,self.dest)
         # self.sprite = self.SPRITES["idle_"+self.direction][0] 
 
-        win.blit(self.sprite,(self.rect.x,self.rect.y) )
+        win.blit(self.sprite,(self.rect.x-offset_x,self.rect.y) )
 
 
 # End of player info
@@ -240,15 +247,15 @@ def get_background(name):
     return tiles, image
 
 
-def draw(window, background,bg_image,player,objects):
+def draw(window, background,bg_image,player,objects,offset_x):
     #this method will draw out the game window
     for tile in background:
         window.blit(bg_image,tile)
 
     for obj in objects:
-        obj.draw(window)
+        obj.draw(window,offset_x)
 
-    player.draw(window)
+    player.draw(window,offset_x)
     pygame.display.update()
 
 
@@ -260,6 +267,8 @@ def main(window):
     pygame.init()
     background, bg_image = get_background("Blue.png")
 
+    offset_x =0
+    scroll_area_width = 200
     player = Player(100,100,60,65)
     block_size = 96
     floor = [Block(i * block_size, HEIGHT - block_size, block_size)  for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
@@ -281,8 +290,9 @@ def main(window):
 
         player.loop(FPS)
         playerMover.handle_move(player,floor)
-        draw(window,background,bg_image,player,floor)
-
+        draw(window,background,bg_image,player,floor,offset_x)
+        if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or ((player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
+            offset_x += player.x_vel
 
     #lets leave the game
     pygame.quit()
